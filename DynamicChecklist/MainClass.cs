18921aeb572f9a36c294a09b1ab5cdb5fbf36cb3
Gunnar <Gunnar@GUNNAR-PC>
2017-03-15 20:01:33 +0100

    moved graph to seperate class

--- DynamicChecklist/MainClass.cs
+++ DynamicChecklist/MainClass.cs
@@ -17,6 +17,7 @@ using QuickGraph.Algorithms;
 using Microsoft.Xna.Framework;
 using QuickGraph.Graphviz;
 using QuickGraph.Graphviz.Dot;
+using DynamicChecklist.Graph;
 
 namespace DynamicChecklist
 {
@@ -104,17 +105,26 @@ namespace DynamicChecklist
         private void ReceiveKeyPress(object sender, EventArgsKeyPressed e)
         {
             //TODO ignore close menu when entering checkbox name
-            if (e.KeyPressed != OpenMenuKey) return;
-            if (Game1.activeClickableMenu is ChecklistMenu)
+            if (e.KeyPressed == OpenMenuKey)
             {
-                Game1.activeClickableMenu = null;
+                if (Game1.activeClickableMenu is ChecklistMenu)
+                {
+                    Game1.activeClickableMenu = null;
+                }
+                else
+                {
+                    objectCollection.update();
+                    ChecklistMenu.objectCollection = objectCollection;
+                    ChecklistMenu.objectLists = objectLists;
+                    ChecklistMenu.Open();
+                }
             }
-            else
+
+            if(e.KeyPressed == Keys.NumPad9)
             {
-                objectCollection.update();
-                ChecklistMenu.objectCollection = objectCollection;
-                ChecklistMenu.objectLists = objectLists;
-                ChecklistMenu.Open();
+                var a = new MyGraph();
+                a.Create();
+                a.Calculate();
             }
 
         }
@@ -136,83 +146,7 @@ namespace DynamicChecklist
             {
                 ol.OnNewDay();
             }
-            var a = Game1.currentLocation.warps;
-          
-            foreach(GameLocation loc in Game1.locations)
-            {
-                //graph.AddVertex(loc.Name);
-            }
-            var partialGraphs = new List<AdjacencyGraph<ExtendedWarp, LabelledEdge<ExtendedWarp>>>();
-            //var edgeCosts = new List<Dictionary<Edge<ExtendedWarp>, double>>();
-            var edgeCost = new Dictionary<LabelledEdge<ExtendedWarp>, double>();
-            foreach (GameLocation loc in Game1.locations)
-            {
-                var partialGraph = new AdjacencyGraph<ExtendedWarp, LabelledEdge<ExtendedWarp>>();
-                var warpsToInclude = new List<Warp>();
-                var extWarpsToInclude = new List<ExtendedWarp>();
-                for (int i = 0; i < loc.warps.Count; i++)
-                {
-                    bool shouldAdd = true;
 
-                    extWarpsToInclude.Add(new ExtendedWarp(loc.warps.ElementAt(i), loc));
-                    partialGraph.AddVertex(extWarpsToInclude.Last());
-                }
-                for (int i = 0; i < loc.warps.Count; i++) 
-                {
-                    var extWarp1 = extWarpsToInclude.ElementAt(i);
-
-
-                    for (int j = i+1; j < loc.warps.Count; j++)
-                    {
-                        // TODO Dont add adjacant warp tiles or make Extended warp be combined from many warps
-                        var LocTo = Game1.getLocationFromName(loc.warps.ElementAt(j).TargetName);
-                        var extWarp2 = extWarpsToInclude.ElementAt(j);
-                        var edge = new LabelledEdge<ExtendedWarp>(extWarp1, extWarp2, extWarp1.Label);
-                        var dist = Vector2.Distance(new Vector2(extWarp1.X, extWarp1.Y), new Vector2(extWarp2.X, extWarp2.Y));
-                        partialGraph.AddEdge(edge);
-                        edgeCost.Add(edge, dist);
-                        
-                    }                   
-                    partialGraph.AddVertex(extWarp1);                 
-                }
-                partialGraphs.Add(partialGraph);
-            }
-            // Combine partial graphs into one
-            var wholeGraph = new MyGraph();
-            foreach (var partialGraph in partialGraphs)
-            {
-                wholeGraph.AddVertexRange(partialGraph.Vertices);
-                wholeGraph.AddEdgeRange(partialGraph.Edges);
-            }
-            for (int i=0; i< partialGraphs.Count; i++)
-            {
-                var graph1 = partialGraphs.ElementAt(i);
-                for (int j=i+1; j < partialGraphs.Count; j++)
-                {                    
-                    var graph2 = partialGraphs.ElementAt(j);
-                    foreach(ExtendedWarp warp1 in graph1.Vertices)
-                    {
-                        foreach(ExtendedWarp warp2 in graph2.Vertices)
-                        {
-                            if(ExtendedWarp.AreCorresponding(warp1, warp2))
-                            {
-                                var edge = new LabelledEdge<ExtendedWarp>(warp1, warp2, warp1.Label);
-                                wholeGraph.AddEdge(edge);
-                                edgeCost.Add(edge, 0);
-                            }
-                        }
-                    }
-                }
-            }
-            GraphvizAlgorithm<ExtendedWarp, LabelledEdge<ExtendedWarp>> graphviz = new GraphvizAlgorithm<ExtendedWarp, LabelledEdge<ExtendedWarp>>(wholeGraph);
-            graphviz.FormatVertex += (sender2, args) => args.VertexFormatter.Comment = args.Vertex.Label;
-            graphviz.FormatEdge += (sender2, args) => { args.EdgeFormatter.Label.Value = args.Edge.Label; };
-            graphviz.ImageType = GraphvizImageType.Jpeg;
-
-            graphviz.Generate(new FileDotEngine(), "C:\\Users\\Gunnar\\Desktop\\graph.png");
-            
-
-            //var alg = new QuickGraph.Algorithms.ShortestPath.UndirectedDijkstraShortestPathAlgorithm<ExtendedWarp, Edge<ExtendedWarp>>;
         }
     }
 
@@ -221,65 +155,5 @@ namespace DynamicChecklist
         public bool val { get; set; } = false;
         public String OpenMenuKey = "NumPad1";
     }
-    public class LabelledEdge<TVertex> : Edge<TVertex>
-    {
-        public string Label;
-        public LabelledEdge(TVertex source, TVertex target, string label) : base(source, target)
-        {
-            this.Label = label;
-        }
-    }
-    public class ExtendedWarp : Warp
-    {
-        public GameLocation OriginLocation;
-        public GameLocation TargetLocation;
-        public string Label;
 
-        public ExtendedWarp(Warp w, GameLocation originLocation): base(w.X, w.Y, w.TargetName, w.TargetX, w.TargetY, false)
-        {
-            this.OriginLocation = originLocation;
-            TargetLocation = Game1.getLocationFromName(w.TargetName);
-            this.Label = w.TargetName + " to " + originLocation.name;
-        }
-
-        public static bool AreCorresponding(ExtendedWarp warp1, ExtendedWarp warp2)
-        {
-            if (warp1.OriginLocation == warp2.TargetLocation && warp1.TargetLocation == warp2.OriginLocation)
-            {
-                if(Math.Abs(warp1.X - warp2.TargetX)<=2 && Math.Abs(warp1.Y - warp2.TargetY)<=2)
-                {
-                    return true;
-                }
-            }
-            return false;
-        } 
-    }
-
-    public class FileDotEngine : IDotEngine
-    {
-        public string Run(GraphvizImageType imageType, string dot, string outputFileName)
-        {
-            //using (StreamWriter writer = new StreamWriter(outputFileName))
-            //{
-            //    writer.Write(dot);
-            //}
-
-            //return System.IO.Path.GetFileName(outputFileName);
-
-            string output = outputFileName;
-            File.WriteAllText(output, dot);
-
-            // assumes dot.exe is on the path:
-            var args = string.Format(@"{0} -Tjpg -O", output);
-            System.Diagnostics.Process.Start(@"C:\Users\Gunnar\Desktop\release\bin\dot.exe", args);
-            return output;
-        }
-    }
-    public class MyGraph : AdjacencyGraph<ExtendedWarp, LabelledEdge<ExtendedWarp>>, IEdgeListGraph<ExtendedWarp, LabelledEdge<ExtendedWarp>>
-    {
-        public MyGraph() : base(false)
-        {
-
-        }
-    }
 }
