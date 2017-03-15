456330eb6df60a063508b7b68b872d4bb019d3b3
Gunnar <Gunnar@GUNNAR-PC>
2017-03-15 20:41:10 +0100

    ongoing refactoring

--- DynamicChecklist/Graph/MyGraph.cs
+++ DynamicChecklist/Graph/MyGraph.cs
@@ -5,20 +5,42 @@ using System.Text;
 using System.Threading.Tasks;
 using QuickGraph.Graphviz;
 using QuickGraph.Graphviz.Dot;
+using QuickGraph.Algorithms.ShortestPath;
 using StardewValley;
 using QuickGraph;
 using System.IO;
 using Microsoft.Xna.Framework;
+using QuickGraph.Algorithms.Observers;
 
 namespace DynamicChecklist.Graph
 {
     public class MyGraph : AdjacencyGraph<ExtendedWarp, LabelledEdge<ExtendedWarp>>, IEdgeListGraph<ExtendedWarp, LabelledEdge<ExtendedWarp>>
     {
+        // IMPORTANT: Rewrite, subclass, organize code. ExtendedWarp makes no sense as neither Vertex nor Edge class. Create better classes for the job (subclass edge, not general);
+        private Dictionary<GameLocation, ExtendedWarp> targetVertices;
+        private Dictionary<GameLocation, ExtendedWarp> playerVertices;
+
+        private Dictionary<GameLocation, PartialGraph> partialGraphs;
+
         public MyGraph() : base(false)
         {
 
         }
+        public void Calculate()
+        {
+            Func<LabelledEdge<ExtendedWarp>, double> edgeCost = (x) => x.Cost;
+            //TryFunc<ExtendedWarp, LabelledEdge<ExtendedWarp>> tryGetPaths = this           
+            var alg = new DijkstraShortestPathAlgorithm<ExtendedWarp, LabelledEdge<ExtendedWarp>>(this, edgeCost);
 
+            var distObserver = new VertexDistanceRecorderObserver<ExtendedWarp, LabelledEdge<ExtendedWarp>>(edgeCost);
+            distObserver.Attach(alg);
+            var predecessorObserver = new VertexPredecessorRecorderObserver<ExtendedWarp, LabelledEdge<ExtendedWarp>>();
+            predecessorObserver.Attach(alg);
+            alg.Compute(this.Vertices.ElementAt(0));
+
+            // Example: Get to town from farm hous
+
+        }
         public void Create()
         {
             var a = Game1.currentLocation.warps;
@@ -27,16 +49,17 @@ namespace DynamicChecklist.Graph
             {
                 //graph.AddVertex(loc.Name);
             }
-            var partialGraphs = new List<AdjacencyGraph<ExtendedWarp, LabelledEdge<ExtendedWarp>>>();
-            //var edgeCosts = new List<Dictionary<Edge<ExtendedWarp>, double>>();
-            var edgeCost = new Dictionary<LabelledEdge<ExtendedWarp>, double>();
+            partialGraphs = new Dictionary<GameLocation, PartialGraph>();
+            //var edgeCosts = new List<Dictionary<Edge<ExtendedWarp>, double>>();          
             foreach (GameLocation loc in Game1.locations)
             {
                 if (loc.Name == "Farm")
                 {
-
+                    // TODO Connect barns/coops
                 }
-                var partialGraph = new AdjacencyGraph<ExtendedWarp, LabelledEdge<ExtendedWarp>>(false);
+                // TODO add player and target vertex and connect every node to it
+                var partialGraph = new PartialGraph();
+                // Calculate which warps should correspond to a vertex. Dont add warps which are very close.
                 var extWarpsToInclude = new List<ExtendedWarp>();
                 for (int i = 0; i < loc.warps.Count; i++)
                 {
@@ -57,18 +80,18 @@ namespace DynamicChecklist.Graph
                     }
 
                 }
+                // Create edges for partial graphs
                 for (int i = 0; i < extWarpsToInclude.Count; i++)
                 {
                     var extWarp1 = extWarpsToInclude.ElementAt(i);
 
 
-                    for (int j = i + 1; j < extWarpsToInclude.Count; j++)
+                    for (int j = 0; j < extWarpsToInclude.Count; j++)
                     {
-                        // TODO Dont add adjacant warp tiles or make Extended warp be combined from many warps
                         var LocTo = Game1.getLocationFromName(loc.warps.ElementAt(j).TargetName);
                         var extWarp2 = extWarpsToInclude.ElementAt(j);
                         var path = PathFindController.findPath(new Point(extWarp1.X, extWarp1.Y), new Point(extWarp2.X, extWarp2.Y), new PathFindController.isAtEnd(PathFindController.isAtEndPoint), loc, Game1.player, 9999);
-                        float dist;
+                        double dist;
                         string edgeLabel;
                         if (path != null)
                         {
@@ -83,52 +106,57 @@ namespace DynamicChecklist.Graph
                             edgeLabel = loc.Name + " - " + dist + "d";
                         }
 
-                        var edge = new LabelledEdge<ExtendedWarp>(extWarp1, extWarp2, edgeLabel, new GraphvizColor(255, 255, 255, 255));
+                        var edge = new LabelledEdge<ExtendedWarp>(extWarp1, extWarp2, edgeLabel, new GraphvizColor(255, 255, 255, 255), dist);
                         partialGraph.AddEdge(edge);
-                        edgeCost.Add(edge, dist);
+                        //edgeCost.Add(edge, dist);
 
                     }
                     partialGraph.AddVertex(extWarp1);
                 }
-                partialGraphs.Add(partialGraph);
+                partialGraph.AddPlayerVertex(new ExtendedWarp(new Warp(0, 0, "None", 0, 0, false), loc));
+                partialGraph.ConnectPlayerVertex();
+                partialGraphs.Add(loc, partialGraph);
             }
             // Combine partial graphs into one
-            var wholeGraph = new MyGraph();
-            foreach (var partialGraph in partialGraphs)
+            foreach (var partialGraph in partialGraphs.Values)
             {
-                wholeGraph.AddVertexRange(partialGraph.Vertices);
-                wholeGraph.AddEdgeRange(partialGraph.Edges);
+                this.AddVertexRange(partialGraph.Vertices);
+                this.AddEdgeRange(partialGraph.Edges);
             }
             for (int i = 0; i < partialGraphs.Count; i++)
             {
-                var graph1 = partialGraphs.ElementAt(i);
-                for (int j = i + 1; j < partialGraphs.Count; j++)
+                var graph1 = partialGraphs.Values.ElementAt(i);
+                for (int j = 0; j < partialGraphs.Count; j++)
                 {
-                    var graph2 = partialGraphs.ElementAt(j);
+                    var graph2 = partialGraphs.Values.ElementAt(j);
                     foreach (ExtendedWarp warp1 in graph1.Vertices)
                     {
                         if (warp1.OriginLocation.name == "Saloon")
                         {
 
                         }
-
-                        foreach (ExtendedWarp warp2 in graph2.Vertices)
+                        if(graph2.VertexCount>0 && warp1.TargetLocation == graph2.Vertices.ElementAt(0).OriginLocation)
                         {
-                            if (warp2.TargetLocation.Name == "Saloon")
-                            {
+                            var copyVertex = new ExtendedWarp((Warp)warp1, warp1.OriginLocation);// copy to make the graph visually clearer
+                            var edgeToCopy = new LabelledEdge<ExtendedWarp>(warp1, copyVertex, "Warp", new GraphvizColor(255, 255, 255, 255), 0);
 
-                            }
-                            if (ExtendedWarp.AreCorresponding(warp1, warp2))
+                            this.AddVertex(copyVertex);
+                            this.AddEdge(edgeToCopy);
+                            foreach (ExtendedWarp warp2 in graph2.Vertices)
                             {
-                                var edge = new LabelledEdge<ExtendedWarp>(warp1, warp2, "Warp", new GraphvizColor(255, 255, 0, 0));
-                                wholeGraph.AddEdge(edge);
-                                edgeCost.Add(edge, 0);
+                                var newEdge = new LabelledEdge<ExtendedWarp>(copyVertex, warp2, warp2.OriginLocation.Name, new GraphvizColor(255, 255, 255, 255), ExtendedWarp.Distance(copyVertex, warp2));
+                                // TODO: Calculate cost
+                                this.AddEdge(newEdge);
+                                if (warp2.TargetLocation?.Name == "Saloon")
+                                {
+
+                                }
                             }
                         }
                     }
                 }
             }
-            GraphvizAlgorithm<ExtendedWarp, LabelledEdge<ExtendedWarp>> graphviz = new GraphvizAlgorithm<ExtendedWarp, LabelledEdge<ExtendedWarp>>(wholeGraph);
+            GraphvizAlgorithm<ExtendedWarp, LabelledEdge<ExtendedWarp>> graphviz = new GraphvizAlgorithm<ExtendedWarp, LabelledEdge<ExtendedWarp>>(this);
             graphviz.FormatVertex += (sender2, args) => args.VertexFormatter.Label = args.Vertex.Label;
             graphviz.FormatEdge += (sender2, args) => { args.EdgeFormatter.Label.Value = args.Edge.Label; };
             graphviz.FormatEdge += (sender2, args) => { args.EdgeFormatter.FontGraphvizColor = args.Edge.Color; };
@@ -140,6 +168,7 @@ namespace DynamicChecklist.Graph
             //var alg = new QuickGraph.Algorithms.ShortestPath.UndirectedDijkstraShortestPathAlgorithm<ExtendedWarp, Edge<ExtendedWarp>>;
         }
     }
+
     public class ExtendedWarp : Warp
     {
         public GameLocation OriginLocation;
@@ -192,12 +221,14 @@ namespace DynamicChecklist.Graph
     }
     public class LabelledEdge<TVertex> : Edge<TVertex>
     {
-        public string Label;
-        public GraphvizColor Color;
-        public LabelledEdge(TVertex source, TVertex target, string label, GraphvizColor color) : base(source, target)
+        public string Label { get; private set; }
+        public GraphvizColor Color { get; private set; }
+        public double Cost { get; set; }
+        public LabelledEdge(TVertex source, TVertex target, string label, GraphvizColor color, double cost) : base(source, target)
         {
             this.Label = label;
             this.Color = color;
+            this.Cost = cost;
         }
     }
 }
