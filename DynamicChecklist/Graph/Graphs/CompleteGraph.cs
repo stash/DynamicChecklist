a4cef12acbeb0c6d6644dbbf818f37c63ac8d897
Gunnar <Gunnar@GUNNAR-PC>
2017-03-25 19:11:07 +0100

    further implementation

--- DynamicChecklist/Graph/Graphs/CompleteGraph.cs
+++ DynamicChecklist/Graph/Graphs/CompleteGraph.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Xna.Framework;
+﻿using DynamicChecklist.Graph.Vertices;
+using Microsoft.Xna.Framework;
+using QuickGraph.Algorithms.Observers;
+using QuickGraph.Algorithms.ShortestPath;
 using StardewValley;
 using System;
 using System.Collections.Generic;
@@ -10,40 +13,131 @@ namespace DynamicChecklist.Graph.Graphs
 {
     public class CompleteGraph : StardewGraph
     {
-        public List<PartialGraph> PartialGraphs { get; private set; }
+        public List<PartialGraph> PartialGraphs { get; private set; } = new List<PartialGraph>();
         private List<GameLocation> gameLocations;
+        private DijkstraShortestPathAlgorithm<StardewVertex, StardewEdge> dijkstra;
+        private VertexDistanceRecorderObserver<StardewVertex, StardewEdge> distObserver;
+        private VertexPredecessorRecorderObserver<StardewVertex, StardewEdge> predecessorObserver;
 
         public CompleteGraph(List<GameLocation> gameLocations)
         {
             this.gameLocations = gameLocations;
         }
-        private void Populate()
+        public void Populate()
         {
             foreach(GameLocation location in gameLocations)
             {
                 var partialGraph = new PartialGraph(location);
                 partialGraph.Populate();
-                AddPartialGraph(partialGraph);
+                PartialGraphs.Add(partialGraph);
             }
-            // TODO Add algorithm
+            foreach (PartialGraph pgSource in PartialGraphs)
+            {
+                foreach (PartialGraph pgTarget in PartialGraphs)
+                {
+                    if (pgSource != pgTarget)
+                    {
+                        ConnectPartialGraph(pgSource, pgTarget);
+                    }                   
+                }
+            }
+            foreach(PartialGraph partialGraph in PartialGraphs)
+            {
+                AddVertexRange(partialGraph.Vertices);
+                AddEdgeRange(partialGraph.Edges);
+            }
+
+            Func<StardewEdge, double> edgeCost = (x) => x.Cost;          
+            dijkstra = new DijkstraShortestPathAlgorithm<StardewVertex, StardewEdge>(this, edgeCost);
+            distObserver = new VertexDistanceRecorderObserver<StardewVertex, StardewEdge>(edgeCost);
+            distObserver.Attach(dijkstra);
+            predecessorObserver = new VertexPredecessorRecorderObserver<StardewVertex, StardewEdge>();
+            predecessorObserver.Attach(dijkstra);
+
         }
-        private void AddPartialGraph(PartialGraph partialGraph)
+        private void ConnectPartialGraph(PartialGraph pgSource, PartialGraph pgTarget)
         {
-            PartialGraphs.Add(partialGraph);
-            // TODO Connect partialGraph to whole graph
+            foreach (StardewVertex vertex in pgSource.Vertices)
+            {
+                if (vertex is WarpVertex)
+                {
+                    var warpVertex = (WarpVertex)vertex;
+                    if (warpVertex.TargetLocation == pgTarget.Location)
+                    {
+                        var newVertex = new StardewVertex(pgTarget.Location, warpVertex.TargetPosition);
+                        pgTarget.AddVertex(newVertex);
+                        var newEdge = new StardewEdge(vertex, newVertex, "Partial graph connection");
+                        pgSource.AddEdge(newEdge);
+                        foreach (StardewVertex targetVertex in pgTarget.Vertices)
+                        {
+                            // Player vertex only needs to connect away from itself, all warp vertices and the target vertex must have an edge going to them
+                            if(targetVertex != pgTarget.PlayerVertex) 
+                            {
+                                var e = new StardewEdge(newVertex, targetVertex, $"From {newVertex.Location} to {targetVertex.Location}");
+                                pgTarget.AddEdge(e);
+                            }
+                        }
+                    }
+
+                }
+            }
         }
-        public void CalculatePathToTarget()
+        public List<Step> CalculatePathToTarget(GameLocation sourceLocation, GameLocation targetLocation)
         {
+            var partialGraphs = FindPartialGraph(sourceLocation);
+            var playerVertex = partialGraphs.PlayerVertex;
+
+            dijkstra.Compute(playerVertex);
             // TODO Figure out return type
+            // TODO Fix bug in graph creation
+            var b = distObserver;
+            var c = predecessorObserver;
+
+            var targetVertex = FindPartialGraph(targetLocation).TargetVertex;
+            var path = (IEnumerable<StardewEdge>)(new List<StardewEdge>());
+            var success = predecessorObserver.TryGetPath(targetVertex, out path);
+            var pathSimple = new List<Step>();
+            if (success)
+            {
+                foreach(var pathPart in path)
+                {
+                    pathSimple.Add(new Step(pathPart.Source.Location, pathPart.Source.Position));
+                }
+                return pathSimple;
+            }
+            else
+            {
+                return null;
+            }
         }
         public void SetTargetLocation(GameLocation location, Vector2 position)
         {
-
+            FindPartialGraph(location).TargetVertex.SetPosition(position);
         }
         public void SetPlayerLocation(GameLocation location, Vector2 position)
         {
-            // TODO Update the corresponding partial graph
+            FindPartialGraph(location).PlayerVertex.SetPosition(position);
         }
-
+        private PartialGraph FindPartialGraph(GameLocation loc)
+        {
+            foreach (PartialGraph p in PartialGraphs)
+            {
+                if(p.Location == loc)
+                {
+                    return p;
+                }
+            }
+            throw new Exception();
+        }
+    }
+}
+public struct Step
+{
+    public GameLocation Location { get; }
+    public Vector2 Position { get; }
+    public Step(GameLocation location, Vector2 position)
+    {
+        Location = location;
+        Position = position;
     }
 }
--- DynamicChecklist/Graph/Graphs/PartialGraph.cs
+++ DynamicChecklist/Graph/Graphs/PartialGraph.cs
@@ -17,18 +17,74 @@ namespace DynamicChecklist.Graph
         public MovableVertex PlayerVertex { get; private set; }
         public MovableVertex TargetVertex { get; private set; }
 
-        public List<StardewEdge> PlayerEdges { get; private set; }
-        public List<StardewEdge> TargetEdges { get; private set; } // only connected from outside the partial graph
+        public List<PlayerEdge> PlayerEdges { get; private set; }
+        //public List<StardewEdge> TargetEdges { get; private set; } // only connected from outside the partial graph
 
         public GameLocation Location { get; private set; }
 
-        public PartialGraph(GameLocation location)
+        public PartialGraph(GameLocation location) : base()
         {
-            
+            Location = location;
         }
         public void Populate()
         {
-            // TODO get code from MyGraph
+            var vertexToInclude = new List<WarpVertex>();
+            var warps = Location.warps;
+            for (int i = 0; i < warps.Count; i++)
+            {
+                var warp = warps.ElementAt(i);
+                var vertexNew = new WarpVertex(Location, new Vector2(warp.X, warp.Y), Game1.getLocationFromName(warp.TargetName), new Vector2(warp.TargetX, warp.TargetY));
+                bool shouldAdd = true;
+                foreach (WarpVertex extWarpIncluded in vertexToInclude)
+                {
+                    if (vertexNew.TargetLocation == extWarpIncluded.TargetLocation && StardewVertex.Distance(vertexNew, extWarpIncluded) < 5)
+                    {
+                        shouldAdd = false;
+                        break;
+                    }
+                }
+                if (shouldAdd)
+                {
+                    vertexToInclude.Add(vertexNew);
+                    
+                    AddVertex(vertexToInclude.Last());
+                }
+
+            }
+            for (int i = 0; i < vertexToInclude.Count; i++)
+            {
+                var vertex1 = vertexToInclude.ElementAt(i);
+
+
+                for (int j = 0; j < vertexToInclude.Count; j++)
+                {
+                    var LocTo = Game1.getLocationFromName(Location.warps.ElementAt(j).TargetName);
+                    var vertex2 = vertexToInclude.ElementAt(j);
+                    var path = PathFindController.findPath(new Point((int)vertex1.Position.X, (int)vertex1.Position.Y), new Point((int)vertex2.Position.X, (int)vertex2.Position.Y), new PathFindController.isAtEnd(PathFindController.isAtEndPoint), Location, Game1.player, 9999);
+                    // TODO Use Pathfinder distance
+                    double dist;
+                    string edgeLabel;
+                    if (path != null)
+                    {
+                        dist = (float)path.Count;
+                        // TODO Player can run diagonally. Account for that.
+                        edgeLabel = Location.Name + " - " + dist + "c";
+
+                    }
+                    else
+                    {
+                        dist = (int)StardewVertex.Distance(vertex1, vertex2);
+                        edgeLabel = Location.Name + " - " + dist + "d";
+                    }
+                    var edge = new StardewEdge(vertex1, vertex2, edgeLabel);
+                    AddEdge(edge);
+
+                }
+                AddVertex(vertex1);
+            }
+            AddPlayerVertex(new MovableVertex(Location, new Vector2(0,0)));
+            AddTargetVertex(new MovableVertex(Location, new Vector2(0, 0)));
+            ConnectPlayerVertex();
         }
         private void AddPlayerVertex(MovableVertex vertex)
         {
@@ -44,6 +100,7 @@ namespace DynamicChecklist.Graph
         }
         private void ConnectPlayerVertex()
         {
+            PlayerEdges = new List<PlayerEdge>();
             foreach (StardewVertex vertex in this.Vertices)
             {
                 if(vertex != PlayerVertex)
@@ -54,25 +111,55 @@ namespace DynamicChecklist.Graph
                 }
             }
         }
+        [Obsolete] // Maybe needed later for pathfinder calculation
         public void UpdatePlayerEdgeCosts(Vector2 position)
         {
             PlayerVertex.SetPosition(position);
             foreach(PlayerEdge edge in PlayerEdges)
             {
-                edge.UpdateCost();
+                
             }
         }
 
         private void AddTargetVertex(MovableVertex w)
         {
-            if (PlayerVertex == null)
+            if (TargetVertex == null)
             {
                 AddVertex(w);
                 TargetVertex = w;
             }
             else
             {
-                throw new InvalidOperationException("Player vertex already added");
+                throw new InvalidOperationException("Target vertex already added");
+            }
+        }
+        public class ExtendedWarp : Warp
+        {
+            public GameLocation OriginLocation;
+            public GameLocation TargetLocation;
+            public string Label;
+
+            public ExtendedWarp(Warp w, GameLocation originLocation) : base(w.X, w.Y, w.TargetName, w.TargetX, w.TargetY, false)
+            {
+                this.OriginLocation = originLocation;
+                TargetLocation = Game1.getLocationFromName(w.TargetName);
+                this.Label = originLocation.name + " to " + w.TargetName;
+            }
+
+            public static bool AreCorresponding(ExtendedWarp warp1, ExtendedWarp warp2)
+            {
+                if (warp1.OriginLocation == warp2.TargetLocation && warp1.TargetLocation == warp2.OriginLocation)
+                {
+                    if (Math.Abs(warp1.X - warp2.TargetX) + Math.Abs(warp1.Y - warp2.TargetY) < 5)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            public static int Distance(ExtendedWarp warp1, ExtendedWarp warp2)
+            {
+                return Math.Abs(warp1.X - warp2.X) + Math.Abs(warp1.Y - warp2.Y);
             }
         }
     }
