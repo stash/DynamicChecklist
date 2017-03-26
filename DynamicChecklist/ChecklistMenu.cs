d31a568e17eb10c6eb778329f2f6d2f523e8a6f9
Gunnar <Gunnar@GUNNAR-PC>
2017-03-26 21:36:33 +0200

    cleaned code up and removed tabs

--- DynamicChecklist/ChecklistMenu.cs
+++ DynamicChecklist/ChecklistMenu.cs
@@ -69,14 +69,11 @@ namespace DynamicChecklist
 
 
         }
-        public override void receiveRightClick(int x, int y, bool playSound = true)
-        {
-        }
+
         public override void receiveKeyPress(Keys key)
         {
             foreach(OptionsElement o in options)
             {
-                // TODO Find out which option is selected
                 o.receiveKeyPress(key);
             }
             base.receiveKeyPress(key);
@@ -84,59 +81,22 @@ namespace DynamicChecklist
         public override void draw(SpriteBatch b)
         {
             drawTextureBox(Game1.spriteBatch, MenuRect.X, MenuRect.Y, MenuRect.Width, MenuRect.Height, Color.White);
-            // Crop menu
             var mouseX = Game1.getMouseX();
             var mouseY = Game1.getMouseY();
 
             int j = 0;
             foreach(ClickableComponent t in tabs)
             {                
-                drawTextureBox(Game1.spriteBatch, t.bounds.X, t.bounds.Y, t.bounds.Width, t.bounds.Height, Color.White* (iSelectedTab == j ? 1F : 0.7F));
-                b.DrawString(Game1.smallFont, t.name, new Vector2(t.bounds.X+5, t.bounds.Y+5), Color.Black);
+                //drawTextureBox(Game1.spriteBatch, t.bounds.X, t.bounds.Y, t.bounds.Width, t.bounds.Height, Color.White* (iSelectedTab == j ? 1F : 0.7F));
+                //b.DrawString(Game1.smallFont, t.name, new Vector2(t.bounds.X+5, t.bounds.Y+5), Color.Black);
                 j++;
-            }
-            switch (selectedTab.name)
-            {
-                case "Checklist":
-                    //drawChecklist(b);
-                    //options.Add(new AddToChecklistElement(300));
-                    break;
-                default:
-                    throw (new NotImplementedException());
             }
             foreach(OptionsElement o in options)
             {
                 o.draw(b, -1, -1);
             }
             base.draw(b);
-            drawHoverText(b, $"{mouseX},{mouseY}", Game1.smallFont);
             drawMouse(b);
-        }
-        private void drawCoopMenu(SpriteBatch b)
-        {
-
-        }
-        private void drawChecklist(SpriteBatch b)
-        {
-
-            //var addNewItemToList = new AddToChecklistElement(300);
-            //addNewItemToList.draw(b, 1, 1);
-
-            foreach (OptionsElement o in options)
-            {
-                o.draw(b, 1, 1);
-            }
-
-        }
-
-        private void drawCrabLine(SpriteBatch b, string locName, int nNeedAction, int nTotal, int line)
-        {
-            var lineHeight = 80;
-
-            var a = Game1.cropSpriteSheet;
-            b.DrawString(Game1.smallFont, $"Location: {locName}, Total: {nTotal}, Baited: {nTotal - nNeedAction}", new Vector2(100 + MenuRect.X, MenuRect.Y + lineHeight * line - 30), Color.Black);
-
-            this.drawHorizontalPartition(b, this.MenuRect.Y + lineHeight * line, true);
         }
 
         private static Rectangle createCenteredRectangle(xTile.Dimensions.Rectangle v, int width, int height)
@@ -145,14 +105,6 @@ namespace DynamicChecklist
             var y = v.Height / 2 - height/2;
             return new Rectangle(x, y, width, height);
 
-        }
-        private Rectangle getSourceRect(Crop crop)
-        {
-            if (crop.dead)
-            {
-                return new Rectangle(192 + 0 % 4 * 16, 384, 16, 32);
-            }
-            return new Rectangle(Math.Min(240, (crop.fullyGrown ? ((crop.dayOfCurrentPhase <= 0) ? 6 : 7) : (((crop.phaseToShow != -1) ? crop.phaseToShow : crop.currentPhase) + ((((crop.phaseToShow != -1) ? crop.phaseToShow : crop.currentPhase) == 0 && 0 % 2 == 0) ? -1 : 0) + 1)) * 16 + ((crop.rowInSpriteSheet % 2 != 0) ? 128 : 0)), crop.rowInSpriteSheet / 2 * 16 * 2, 16, 32);
         }
 
         public override void receiveLeftClick(int x, int y, bool playSound = true)
@@ -163,7 +115,6 @@ namespace DynamicChecklist
                 if (tabs[i].bounds.Contains(x, y))
                 {
                     iSelectedTab = i;
-                    Game1.playSound("dwop");
                     Game1.activeClickableMenu = new ChecklistMenu();
                 }
 
@@ -177,13 +128,14 @@ namespace DynamicChecklist
 
             }
         }
-        public override void releaseLeftClick(int x, int y)
-        {
-            base.releaseLeftClick(x, y);
-        }
         public static void Open()
         {
             Game1.activeClickableMenu = new ChecklistMenu();
+        }
+
+        public override void receiveRightClick(int x, int y, bool playSound = false)
+        {
+
         }
     }
 
