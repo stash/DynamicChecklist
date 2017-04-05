a8abf7cadd264ec2d17ee9b314fde7cf23d5b4ec
Gunnar <Gunnar@GUNNAR-PC>
2017-04-05 23:00:58 +0200

    fix

--- DynamicChecklist/Options/DCOptionsCheckbox.cs
+++ DynamicChecklist/Options/DCOptionsCheckbox.cs
@@ -9,11 +9,13 @@
 
     public class DCOptionsCheckbox : OptionsCheckbox
     {
+        private int whichOption2; // Needed so that the receiveLeftClick base function doesn't change some game option
         private ModConfig config;
 
         public DCOptionsCheckbox(string label, int whichOption, ModConfig config, int x = -1, int y = -1)
-            : base(label, 1, x, y)
+            : base(label, 9999, x, y)
         {
+            this.whichOption2 = whichOption;
             this.config = config;
             switch (whichOption)
             {
@@ -24,10 +26,10 @@
                     this.isChecked = config.AllowMultipleOverlays;
                     break;
                 case 5:
-                    this.isChecked = config.AllowMultipleOverlays;
+                    this.isChecked = config.ShowArrow;
                     break;
                 case 6:
-                    this.isChecked = config.AllowMultipleOverlays;
+                    this.isChecked = config.ShowOverlay;
                     break;
                 default:
                     throw new NotImplementedException();
@@ -37,8 +39,7 @@
         public override void receiveLeftClick(int x, int y)
         {
             base.receiveLeftClick(x, y);
-            this.isChecked = !this.isChecked;
-            switch (this.whichOption)
+            switch (this.whichOption2)
             {
                 case 3:
                     this.config.ShowAllTasks = this.isChecked;
@@ -47,11 +48,13 @@
                     this.config.AllowMultipleOverlays = this.isChecked;
                     break;
                 case 5:
-                    this.config.AllowMultipleOverlays = this.isChecked;
+                    this.config.ShowArrow = this.isChecked;
                     break;
                 case 6:
-                    this.config.AllowMultipleOverlays = this.isChecked;
+                    this.config.ShowOverlay = this.isChecked;
                     break;
+                default:
+                    throw new NotImplementedException();
             }
         }
     }
