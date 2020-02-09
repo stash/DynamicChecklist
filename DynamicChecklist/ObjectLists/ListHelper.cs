b5031224171ee8ffda4381b9b7e7659335272cef
Jeremy Stashewsky <jstash@gmail.com>
2020-02-09 13:06:52 -0800

    Load object names and categories

--- DynamicChecklist/ObjectLists/ListHelper.cs
+++ DynamicChecklist/ObjectLists/ListHelper.cs
@@ -13,6 +13,36 @@
 
     public class ListHelper
     {
+        private static Dictionary<int, string> objectNames = null;
+
+        private static Dictionary<int, int> objectCategories = null;
+
+        internal static Dictionary<int, string> ObjectNames
+        {
+            get
+            {
+                if (objectNames == null)
+                {
+                    PopulateObjectsNames();
+                }
+
+                return objectNames;
+            }
+        }
+
+        internal static Dictionary<int, int> ObjectCategories
+        {
+            get
+            {
+                if (objectCategories == null)
+                {
+                    PopulateObjectsNames();
+                }
+
+                return objectCategories;
+            }
+        }
+
         public static IEnumerable<AnimalHouse> GetFarmAnimalHouses()
         {
             foreach (Building b in Game1.getFarm().buildings)
@@ -30,6 +60,34 @@
             foreach (var pair in objects.Pairs)
             {
                 yield return pair;
+            }
+        }
+
+        internal static void PopulateObjectsNames()
+        {
+            if (objectCategories != null)
+            {
+                objectCategories.Clear();
+            }
+            else
+            {
+                objectCategories = new Dictionary<int, int>();
+            }
+
+            if (objectNames != null)
+            {
+                objectNames.Clear();
+            }
+            else
+            {
+                objectNames = new Dictionary<int, string>();
+            }
+
+            foreach (var pair in Game1.objectInformation)
+            {
+                ObjectNames[pair.Key] = pair.Value.Split("/".ToCharArray())[0];
+                var o = new StardewValley.Object(pair.Key, 0);
+                ObjectCategories[pair.Key] = o.Category;
             }
         }
     }
